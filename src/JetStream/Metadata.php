467794d775c580bbba0ff6a216c187840f5ae44a
kafkiansky <vadimzanfir@gmail.com>
2025-05-23 11:00:21 +0300

    Parse Metadata for jetstream consumers

---
+++ src/JetStream/Metadata.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Nats\JetStream;
+
+use Thesis\Time\TimeSpan;
+
+/**
+ * @api
+ */
+final readonly class Metadata
+{
+    public function __construct(
+        public string $stream,
+        public string $consumer,
+        public int $delivered,
+        public int $streamSequence,
+        public int $consumerSequence,
+        public \DateTimeImmutable $timestamp,
+        public int $pending,
+        public ?string $domain = null,
+        public ?string $hash = null,
+        public ?string $token = null,
+    ) {}
+
+    /**
+     * Parse jetstream consumer's reply subject in one of the following forms:
+     * <code>
+     * $JS.ACK.<stream>.<consumer>.<delivered>.<sseq>.<cseq>.<tm>.<pending>
+     * $JS.ACK.<domain>.<account hash>.<stream>.<consumer>.<delivered>.<sseq>.<cseq>.<tm>.<pending>?.<a token with a random value>
+     * </code>
+     *
+     * @internal
+     * @param non-empty-string $subject
+     * @throws \InvalidArgumentException
+     */
+    public static function parse(string $subject): self
+    {
+        $prefix = '$JS.ACK.';
+
+        if (!str_starts_with($subject, $prefix)) {
+            throw new \InvalidArgumentException('Invalid subject format: no leading "$JS.ACK." prefix.');
+        }
+
+        $tokens = explode('.', substr($subject, strlen($prefix)));
+        $length = \count($tokens);
+
+        if ($length < 7 || ($length > 7 && $length < 9)) {
+            throw new \InvalidArgumentException('Invalid subject format: incorrect length.');
+        }
+
+        if ($length === 7) {
+            array_unshift($tokens, null, null);
+        }
+
+        /**
+         * @var array{
+         *     0: ?string,
+         *     1: ?string,
+         *     2: string,
+         *     3: string,
+         *     4: string,
+         *     5: string,
+         *     6: string,
+         *     7: string,
+         *     8: string,
+         *     9?: string,
+         * } $tokens
+         */
+
+        return new self(
+            stream: $tokens[2],
+            consumer: $tokens[3],
+            delivered: (int) $tokens[4],
+            streamSequence: (int) $tokens[5],
+            consumerSequence: (int) $tokens[6],
+            timestamp: (new \DateTimeImmutable())->setTimestamp(
+                TimeSpan::fromNanoseconds((int) $tokens[7])->toSeconds(),
+            ),
+            pending: (int) $tokens[8],
+            domain: $tokens[0],
+            hash: $tokens[1],
+            token: $tokens[9] ?? null,
+        );
+    }
+}
---
+++ tests/JetStream/MetadataTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Nats\JetStream;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\TestWith;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Metadata::class)]
+final class MetadataTest extends TestCase
+{
+    /**
+     * @param non-empty-string $subject
+     */
+    #[TestWith(
+        [
+            '$JS.ACK.EventsStream.EventsConsumer.1.1.1.1747976767996376564.0',
+            new Metadata(
+                stream: 'EventsStream',
+                consumer: 'EventsConsumer',
+                delivered: 1,
+                streamSequence: 1,
+                consumerSequence: 1,
+                timestamp: new \DateTimeImmutable('@1747976768'),
+                pending: 0,
+            ),
+        ],
+    )]
+    #[TestWith(
+        [
+            '$JS.ACK.test.xxx.EventsStream.EventsConsumer.1.1.1.1747976767996376564.0',
+            new Metadata(
+                stream: 'EventsStream',
+                consumer: 'EventsConsumer',
+                delivered: 1,
+                streamSequence: 1,
+                consumerSequence: 1,
+                timestamp: new \DateTimeImmutable('@1747976768'),
+                pending: 0,
+                domain: 'test',
+                hash: 'xxx',
+            ),
+        ],
+    )]
+    #[TestWith(
+        [
+            '$JS.ACK.test.xxx.EventsStream.EventsConsumer.1.1.1.1747976767996376564.0.random',
+            new Metadata(
+                stream: 'EventsStream',
+                consumer: 'EventsConsumer',
+                delivered: 1,
+                streamSequence: 1,
+                consumerSequence: 1,
+                timestamp: new \DateTimeImmutable('@1747976768'),
+                pending: 0,
+                domain: 'test',
+                hash: 'xxx',
+                token: 'random',
+            ),
+        ],
+    )]
+    public function testParseMetadata(string $subject, Metadata $metadata): void
+    {
+        self::assertEquals($metadata, Metadata::parse($subject));
+    }
+
+    public function testParseInvalidPrefix(): void
+    {
+        self::expectExceptionObject(new \InvalidArgumentException('Invalid subject format: no leading "$JS.ACK." prefix.'));
+        Metadata::parse('EventsStream.EventsConsumer.1.1.1.1747976767996376564.0');
+    }
+
+    public function testParseInvalidLength(): void
+    {
+        self::expectExceptionObject(new \InvalidArgumentException('Invalid subject format: incorrect length.'));
+        Metadata::parse('$JS.ACK.EventsStream.EventsConsumer.1.1.1');
+    }
+}
