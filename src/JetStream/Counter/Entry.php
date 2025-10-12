c71e4bdf5d73f46d69cf0e38d42224245f97d8d6
kafkiansky <vadimzanfir@gmail.com>
2025-10-12 08:22:58 +0300

    Remove sources from Entry

--- src/JetStream/Counter/Entry.php
+++ src/JetStream/Counter/Entry.php
@@ -11,12 +11,10 @@ final readonly class Entry
 {
     /**
      * @param non-empty-string $subject
-     * @param array<non-empty-string, array<non-empty-string, int>> $sources
      */
     public function __construct(
         public string $subject,
         public int $value,
         public int $incr,
-        public array $sources = [],
     ) {}
 }
