fc2ecb3963171f19a824f31fdb09890f09efc7a2
Petriashin <aleksandr.petriashin@b2broker.com>
2025-10-23 17:26:49 +0300

    chore: integration tests with jwt

---
+++ tests/JwtNkeyIntegrationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thesis\Nats;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\RequiresPhpExtension;
+
+#[CoversClass(Client::class)]
+#[CoversClass(JetStream::class)]
+#[RequiresPhpExtension('sodium')]
+final class JwtNkeyIntegrationTest extends NatsTestCase
+{
+    public function testJwtWithNkeyConnection(): void
+    {
+        $client = $this->clientWithJwtAuth();
+        $js = $client->jetStream();
+
+        $info = $js->accountInfo();
+
+        self::assertNotEmpty($info);
+    }
+}
